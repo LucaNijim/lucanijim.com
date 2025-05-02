2fd466d797a8de20d5aa556cfa439633aeef4430
Luca <lnijim@nd.edu>
2025-05-02 11:05:05 +0100

    changed sitemap one more time for seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://lucanijim.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap({
+			lastmod: new Date('2025-05-02')
+		})
+	],
 });
